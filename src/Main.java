e4973b1d1b5644178edf57f55a34a8629762cd4a
Ramouth <42993992+Ramouth@users.noreply.github.com>
2018-10-26 17:41:20 +0200

    Arrays til at starte med

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+     public static void main(String[] args) {
+         String[] anArray = {
+                 "Nokotahest", "Slesvigsk Koldblod", "Pampa horse",
+                 "Mustang", "Islandsk hest"
+         };
+ }
+}
